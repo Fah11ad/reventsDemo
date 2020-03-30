f0cbd2312a7040f1e96868c01e53e10ad5a8c3a9
Fah11ad <falqurashi11@gmail.com>
2020-03-30 20:52:07 +0300

    End of section 8

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import testReducer from "../../features/testarea/testReducer";
+import eventReducer from "../../features/event/eventReducer";
 
-
+//section 7 4th => add the reducer to the root reducer
 const rootReducer = combineReducers({
-    test: testReducer
+    test: testReducer,
+    events: eventReducer
 })
 export default rootReducer
