f7a4023c9ca1496a858273dc0bf2d497c9225c7f
Fah11ad <falqurashi11@gmail.com>
2020-04-09 20:48:16 +0300

    End of Section 9

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import { reducer as FormReducer } from 'redux-form'
 import testReducer from "../../features/testarea/testReducer";
 import eventReducer from "../../features/event/eventReducer";
 
 //section 7 4th => add the reducer to the root reducer
 const rootReducer = combineReducers({
+    form: FormReducer,
     test: testReducer,
     events: eventReducer
 })
